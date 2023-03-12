27ff339703fd3a960655665d4ab288ac1674473e
Friosn <franz.rios.nino@gmail.com>
2023-03-12 11:23:43 +0100

    Added data characters

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Constants from 'expo-constants';
 import { Text, View } from 'react-native';
-import { Link, Route, Redirect } from 'react-router-native';
+import { Link, Route, Redirect, Switch } from 'react-router-native';
 
 const Main = () => {
   return (
     <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
-      <Text>First React Native App</Text>
+      {/*  <Switch>
+        <Route path="/" exact></Route>
+        <Route path="/signin" exact></Route>
+      </Switch> */}
     </View>
   );
 };
---
+++ src/data/characters.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    id: 'protagonist',
+    fullName: 'Clancy Gilroy',
+    description: 'Space-cast creator and interviewer',
+    mainValue: 'Introspection',
+    voice: 'Duncan Trussel',
+    chapters: 8,
+    death: 'Simulator explosion',
+    avatarUrl:
+      'https://res.cloudinary.com/dfrmvbvdc/image/upload/v1665408499/Midnight%20Gospel/clancy_gilroy_rtbro4.jpg',
+  },
+  {
+    id: 'chuck.ep.1',
+    fullName: 'Chuck Charles',
+    description: 'Mall saviour and dead hero',
+    mainValue: 'Get in!',
+    voice: 'Joey Diaz',
+    chapters: 1,
+    death: 'Butchered by zombos',
+    avatarUrl:
+      'https://res.cloudinary.com/dfrmvbvdc/image/upload/v1665408489/Midnight%20Gospel/chuck_charles_y1832y.png',
+  },
+  {
+    id: 'president.ep.1',
+    fullName: 'Mr. President',
+    description: 'President of the united zombolands',
+    mainValue: 'There is not absolute good and bad, but circunstances',
+    voice: 'Drew Pinsky',
+    chapters: 1,
+    death: 'Zombo bites',
+    avatarUrl:
+      'https://res.cloudinary.com/dfrmvbvdc/image/upload/v1665408249/Midnight%20Gospel/glasses_man_box4gp.png',
+  },
+  {
+    id: 'annie.ep.2',
+    fullName: 'Annie Deer Dog',
+    description: 'Duel helper and supporter',
+    mainValue: 'Acceptance',
+    voice: 'Anne Lamott',
+    chapters: 1,
+    death: 'Meat factory smash',
+    avatarUrl:
+      'https://res.cloudinary.com/dfrmvbvdc/image/upload/v1665408266/Midnight%20Gospel/annie_cav5ro.jpg',
+  },
+];
